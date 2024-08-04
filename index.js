ddb82281c03a9826fc3817eb15f1d93c937d596a
juthaluck-s <juthaluck.ss@gmail.com>
2024-08-04 17:55:15 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,277 @@
+var product = [{
+   id: 1,
+   img: 'https://assets.millionpugs.com/redeem/prd/Valorant_Points_75_and_475_VP_8bcb75f591.png',
+   name: '540 Vp',
+   price: 120,
+   description:'เติม Valorant แบบพรีออเดอร์ใช้เวลาในการเติม 1-24 ชั่วโมง ใช้แค่ชื่อในเกม(flashplay#1234)',
+   type:'valorant'
+},{
+   id: 2,
+   img: 'https://assets.millionpugs.com/redeem/prd/Valorant_Points_75_and_475_VP_8bcb75f591.png',
+   name: '1130 Vp',
+   price: 230,
+   description:'เติม Valorant แบบพรีออเดอร์ใช้เวลาในการเติม 1-24 ชั่วโมง ใช้แค่ชื่อในเกม(flashplay#1234',
+   type:'valorant'
+},{
+   id: 3,
+   img: 'https://assets.millionpugs.com/redeem/prd/Valorant_Points_75_and_475_VP_8bcb75f591.png',
+   name: '1945 Vp',
+   price: 380,
+   description:'เติม Valorant แบบพรีออเดอร์ใช้เวลาในการเติม 1-24 ชั่วโมง ใช้แค่ชื่อในเกม(flashplay#1234)',
+   type:'valorant'
+},{
+   id: 4,
+   img: 'https://assets.millionpugs.com/redeem/prd/Valorant_Points_75_and_475_VP_8bcb75f591.png',
+   name: '2485 Vp',
+   price: 500,
+   description:'เติม Valorant แบบพรีออเดอร์ใช้เวลาในการเติม 1-24 ชั่วโมง ใช้แค่ชื่อในเกม(flashplay#1234)',
+   type:'valorant'
+},{
+   id: 5,
+   img: 'https://assets.millionpugs.com/redeem/prd/Valorant_Points_75_and_475_VP_8bcb75f591.png',
+   name: '3390 Vp',
+   price: 690,
+   description:'เติม Valorant แบบพรีออเดอร์ใช้เวลาในการเติม 1-24 ชั่วโมง ใช้แค่ชื่อในเกม(flashplay#1234)',
+   type:'valorant'
+},{
+   id: 6,
+   img: 'https://assets.millionpugs.com/redeem/prd/Valorant_Points_75_and_475_VP_8bcb75f591.png',
+   name: '3890 Vp',
+   price: 760,
+   description:'เติม Valorant แบบพรีออเดอร์ใช้เวลาในการเติม 1-24 ชั่วโมง ใช้แค่ชื่อในเกม(flashplay#1234)',
+   type:'valorant'
+},{
+    id: 7,
+    img: 'https://cdn1.epicgames.com/fn/offer/EGS_1000VBucks_1920x1080-1920x1080-874f1246563fa2e19689c280b78f9ac3ca04eadf.png',
+    name: '1000 Vbucks',
+    price: 120,
+    description: 'ราคา Fortnite เติมรวดเร็ว ไม่ต้องรอคิวบริการรับเติมราคาถูกกว่าเติมเอง !!!โปรโมชั่นลดราคา! ปลอดภัย เติมไวลูกค้าต้องเชื่อม Xbox Live กับบัญชี Epic game ของตัวเองถึงจะทำการเติม',
+    type: 'fortnite'
+},{
+    id: 8,
+    img: 'https://cdn1.epicgames.com/fn/offer/EGS_1000VBucks_1920x1080-1920x1080-874f1246563fa2e19689c280b78f9ac3ca04eadf.png',
+    name: '2800 Vbucks',
+    price: 199,
+    description: 'ราคา Fortnite เติมรวดเร็ว ไม่ต้องรอคิวบริการรับเติมราคาถูกกว่าเติมเอง !!!โปรโมชั่นลดราคา! ปลอดภัย เติมไวลูกค้าต้องเชื่อม Xbox Live กับบัญชี Epic game ของตัวเองถึงจะทำการเติม',
+    type: 'fortnite'
+},{
+    id: 9,
+    img: 'https://cdn1.epicgames.com/fn/offer/EGS_1000VBucks_1920x1080-1920x1080-874f1246563fa2e19689c280b78f9ac3ca04eadf.png',
+    name: '5000 Vbucks',
+    price: 290,
+    description: 'ราคา Fortnite เติมรวดเร็ว ไม่ต้องรอคิวบริการรับเติมราคาถูกกว่าเติมเอง !!!โปรโมชั่นลดราคา! ปลอดภัย เติมไวลูกค้าต้องเชื่อม Xbox Live กับบัญชี Epic game ของตัวเองถึงจะทำการเติม',
+    type: 'fortnite'
+},{
+    id: 10,
+    img: 'https://cdn1.epicgames.com/fn/offer/EGS_1000VBucks_1920x1080-1920x1080-874f1246563fa2e19689c280b78f9ac3ca04eadf.png',
+    name: '13500 Vbucks',
+    price: 420,
+    description: 'ราคา Fortnite เติมรวดเร็ว ไม่ต้องรอคิวบริการรับเติมราคาถูกกว่าเติมเอง !!!โปรโมชั่นลดราคา! ปลอดภัย เติมไวลูกค้าต้องเชื่อม Xbox Live กับบัญชี Epic game ของตัวเองถึงจะทำการเติม',
+    type: 'fortnite'
+},{
+    id: 11,
+    img: 'https://blz-contentstack-images.akamaized.net/v3/assets/bltf408a0557f4e4998/blt5387d7cefaf9923d/6334e1997e84e17596f9c816/Coins_960x540.png',
+    name: '500 Coin',
+    price: 150,
+    description: 'บริการรับเติม Overwatch 2 ราคาถูก กว่าเติมเอง 30-70% ลูกค้าต้องเชื่อม Xbox Live กับบัญชี Battle.Net',
+    type: 'overwatch2'
+},{
+    id: 12,
+    img: 'https://blz-contentstack-images.akamaized.net/v3/assets/bltf408a0557f4e4998/blt5387d7cefaf9923d/6334e1997e84e17596f9c816/Coins_960x540.png',
+    name: '1000 Coin',
+    price: 250,
+    description: 'บริการรับเติม Overwatch 2 ราคาถูก กว่าเติมเอง 30-70% ลูกค้าต้องเชื่อม Xbox Live กับบัญชี Battle.Net',
+    type: 'overwatch2'
+},{
+    id: 13,
+    img: 'https://blz-contentstack-images.akamaized.net/v3/assets/bltf408a0557f4e4998/blt5387d7cefaf9923d/6334e1997e84e17596f9c816/Coins_960x540.png',
+    name: '2200 Coin',
+    price: 500,
+    description: 'บริการรับเติม Overwatch 2 ราคาถูก กว่าเติมเอง 30-70% ลูกค้าต้องเชื่อม Xbox Live กับบัญชี Battle.Net',
+    type: 'overwatch2'
+},{
+    id: 14,
+    img: 'https://blz-contentstack-images.akamaized.net/v3/assets/bltf408a0557f4e4998/blt5387d7cefaf9923d/6334e1997e84e17596f9c816/Coins_960x540.png',
+    name: '5700 Coin',
+    price: 1050,
+    description: 'บริการรับเติม Overwatch 2 ราคาถูก กว่าเติมเอง 30-70% ลูกค้าต้องเชื่อม Xbox Live กับบัญชี Battle.Net',
+    type: 'overwatch2'
+},{
+    id: 15,
+    img: 'https://blz-contentstack-images.akamaized.net/v3/assets/bltf408a0557f4e4998/blt5387d7cefaf9923d/6334e1997e84e17596f9c816/Coins_960x540.png',
+    name: '11600 Coin',
+    price: 2200,
+    description: 'บริการรับเติม Overwatch 2 ราคาถูก กว่าเติมเอง 30-70% ลูกค้าต้องเชื่อม Xbox Live กับบัญชี Battle.Net',
+    type: 'overwatch2'
+}]
+//,{},{},{},{},{},{},{},{}]
+
+$(document).ready (()=> {
+    var html='';
+    for (let i = 0; i < product.length; i++) {
+        html += `<div onclick="openProductDetail(${i})" class="product-items ${product[i].type}">
+                <img class="product-img" src="${product[i].img}" alt="">
+                <p style="font-size: 1.2vw;">${product[i].name}</p>
+                <p style="font-size: 0.9vw;">${numberWithCommas (product[i].price)} THB</p>
+            </div>`;  
+    }
+    $("#productlist").html(html);
+})
+
+function numberWithCommas(x) {
+    x = x.toString();
+    var pattern = /(-?\d+)(\d{3})/;
+    while (pattern.test(x))
+        x = x.replace(pattern, "$1,$2");
+    return x;
+}
+
+function searchsomething(elem) {
+    var value = $('#'+elem.id).val()
+    console.log(value)
+
+    var html='';
+    for (let i = 0; i < product.length; i++) {
+        if ( product[i].name.includes(value) ) {
+            html += `<div onclick="openProductDetail(${i})" class="product-items ${product[i].type}">
+                <img class="product-img" src="${product[i].img}" alt="">
+                <p style="font-size: 1.2vw;">${product[i].name}</p>
+                <p style="font-size: 0.9vw;">${numberWithCommas (product[i].price)} THB</p>
+            </div>`;  
+        } 
+    }
+    if(html == '') {
+        $("#productlist").html(`<p>Not found</p>`);
+    } else {
+        $("#productlist").html(html);
+    }
+}
+
+    function searchproduct(param) {
+        console.log(param)
+        $(".product-items").css('display','none')
+        if(param == 'all') {
+            $(".product-items").css('display','block')
+        } else {
+            $("."+param).css('display', 'block')
+        }
+    }
+
+    var productindex = 0;
+    function openProductDetail(index) {
+        productindex = index;
+        console.log(productindex)
+        $("#modalDesc").css('display', 'flex')
+        $("#mdd-img").attr('src', product[index].img); 
+        $("#mdd-name").text(product[index].name); 
+        $("#mdd-price").text(numberWithCommas(product[index].price) +' THB'); 
+        $("#mdd-desc").text(product[index].description);    
+    }
+
+    function closeModal() {
+        $(".modal").css('display', 'none')
+    }
+
+    var cart = [];
+    function addtocart() {
+        var pass = true;
+        for (let i = 0; i < cart.length; i++) {
+            if(productindex == cart[i].index) {
+                console.log('found same product')
+                cart[i].count++;
+                pass = false;
+            }
+        }
+
+        if(pass) {
+            var obj = {
+                index: productindex,
+                id: product[productindex].id,
+                name: product[productindex].name,
+                price: product[productindex].price,
+                img: product[productindex].img,
+                count: 1
+            };
+            console.log(obj)
+            cart.push(obj)
+        }
+        console.log(cart)
+        
+        Swal.fire ({
+            icon: 'success',
+            title: 'Add ' + product[productindex].name+ ' to cart !'
+        })
+        $("#cartcount").css('display', 'flex').text(cart.length)
+
+    }
+
+    function openCart() {
+        $('#modalCart').css('display', 'flex')
+        rendercart();
+    }
+
+    function rendercart() {
+        if(cart.length > 0) {
+            var html = '';
+            for (let i = 0; i < cart.length; i++) {
+                html += `<div class="cartlist-items">
+                            <div class="cartlist-left">
+                                <img src="${cart[i].img}" alt="">
+                                <div class="cartlist-detail">
+                                    <p style="font-size: 1.5vx;">${cart[i].name}</p>
+                                    <p style="font-size: 1.2vx;">${numberWithCommas(cart[i].price * cart[i].count)} THB</p>
+                                </div>
+                            </div>
+                            <div class="cartlist-right">
+                                <p onclick="deinitems('-', ${i})" class="btnc">-</p>
+                                <p id="countitems${i}" style="margin: 0 15px;">${cart[i].count}</p>
+                                <p onclick="deinitems('+', ${i})" class="btnc">+</p>
+                            </div>
+                        </div>`;
+                
+            }
+            $("#mycart").html(html)
+        }
+        else {
+            $("#mycart").html(`<p>Not  found product list</p>`)
+        }
+    }
+    
+    function deinitems(action, index) {
+        if(action == '-') {
+            if(cart[index].count > 0) {
+                cart[index].count--;
+                $("#countitems"+index).text(cart[index].count)
+
+                if(cart[index].count <= 0) {
+                    Swal.fire({
+                        icon: 'warning',
+                        title: 'Are you sure to delete',
+                        showConfirmButton: true,
+                        showCancelButton: true,
+                        confirmButtonText: 'Delete',
+                        cancelButtonText: 'Cancel'
+                    }).then((res) => {
+                        if(res.isConfirmed) {
+                            cart.splice(index, 1)
+                            console.log(cart)
+                            rendercart();
+                            $("#cartcount").css('display', 'flex').text(cart.length)
+                            
+                            if(cart.length <=0) {
+                                $("#cartcount").css('display', 'none')
+                            }
+                        }
+                        else {
+                            cart[index].count++;
+                            $("#countitems"+index).text(cart[index].count)
+                        }
+                    })
+                }
+            }
+        }
+        else if(action == '+') {
+            cart[index].count++;
+            $("#countitems"+index).text(cart[index].count)
+        }
+    }
